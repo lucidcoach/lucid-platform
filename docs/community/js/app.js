dea4cb3c8586c3fd85e988ccb07506779915f2f3
LucidCoach <shineast2@gmail.com>
2026-09-04 12:21:38 +0900

    Add files via upload

--- docs/community/js/app.js
+++ docs/community/js/app.js
@@ -3,7 +3,7 @@ import { $ } from "./utils.js?v=20260904r";
 import { switchView } from "./view.js?v=20260904r";
 import { loadRecent } from "./pages/recentMatches.js?v=20260904r";
 import { state } from "./state.js?v=20260904r";
-import { openPlayer, searchPlayers } from "./pages/playerSearch.js?v=20260904r";
+import { openPlayer, searchPlayers } from "./pages/playerSearch.js?v=20260904s";
 
 
 const RECENT_SEARCH_KEY = "lucid-community-recent-searches-v2";
