52c088e39d17e0e83cf861af6c5c64fb649e354e
LucidCoach <shineast2@gmail.com>
2026-09-13 16:18:21 +0900

    feat: reorganize admin economy layout

--- docs/community/js/pages/communityAdmin.js
+++ docs/community/js/pages/communityAdmin.js
@@ -1,7 +1,7 @@
 
 import { getCurrentUser, isCommunityAdmin } from "../auth.js?v=20260911authsingleton1";
 import { API_BASE_URL } from "../config.js?v=20260904d";
-import { renderMileage } from "./mileage.js?v=20260913progress3";
+import { renderMileage } from "./mileage.js?v=20260913economy1";
 import { championIcon } from "../assets.js?v=20260904r";
 
 const esc=(value)=>String(value??"").replace(/[&<>"']/g,ch=>({"&":"&amp;","<":"&lt;",">":"&gt;",'"':"&quot;","'":"&#39;"}[ch]));
